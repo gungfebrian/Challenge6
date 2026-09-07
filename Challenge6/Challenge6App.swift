59459a04a598a9726e9a7236d09130ef7d7d1fe1
Gung <guefef17@gmail.com>
2026-09-07 23:05:40 +0800

    Initial Commit

---
+++ Challenge6/Challenge6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6App.swift
+//  Challenge6
+//
+//  Created by Gung  on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
